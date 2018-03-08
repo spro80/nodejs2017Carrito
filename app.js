95dc3d2d7f3cea1ca8372141664393014ee6262a
David Diaz <david.diaz@zweicom.com>
2018-03-08 15:25:17 -0300

    se agregan configuraciones

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hola Mundo');
+////////////////////////////////////////
+//////////Require node_modules//////////
+////////////////////////////////////////
+//var express = require('express');
+//var routes = require('./src/routes');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+//var mysql = require('mysql');
+
+
+////////////////////////////////////////
+////////Require internal modules////////
+////////////////////////////////////////
+var connection = require("./src/config/config.js");
+var app = require("./src/config/configExpress.js");
+
+require('./src/routes/routes')(app);
+
+
+
+
+
+
+
+////////////////////////////////////////
+///////Load Variable Environment////////
+////////////////////////////////////////
+//var ConfigEntorno = require('./src/config/envVariables.js');
+var ConfigEntorno = require('./src/config/environmentConfig.js');
+let ConfigEntornoAmbiente = ConfigEntorno.EnvironmentConfig();
+console.log( ConfigEntornoAmbiente.PORT );
+console.log( ConfigEntornoAmbiente.SERVERURL );
+
+
+
+app.configure(function() {
+  app.set('connection', connection);
+  //app.set('app', app);
 });
 
-app.get('/inicio', function (req, res) {
-  res.send('Inicio');
+
+
+http.createServer(app).listen( ConfigEntornoAmbiente.PORT , function(){
+  console.log('Express Server Listening ON Port ' + ConfigEntornoAmbiente.PORT );
 });
 
-app.get('/menu', function (req, res) {
-  res.send('Menu');
-});
 
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node corre en el puerto ', app.get('port'));
-});
