4202ef6a092a73e4dbf33f41c3f823c4eb5895fb
David Diaz <david.diaz@zweicom.com>
2018-03-08 11:58:10 -0300

    se agregan nuevas rutas

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.get('/', function (req, res) {
   res.send('Hola Mundo');
 });
 
+app.get('/inicio', function (req, res) {
+  res.send('Inicio');
+});
+
+app.get('/menu', function (req, res) {
+  res.send('Menu');
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node corre en el puerto ', app.get('port'));
 });
