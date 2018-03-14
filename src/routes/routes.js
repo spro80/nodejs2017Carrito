856477f0bb9868895f3df2e2a4ccbc32b3fbb230
David Diaz <david.diaz@zweicom.com>
2018-03-14 11:56:05 -0300

    se agregan nuevos routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ module.exports = function ( app ){
 	console.log(app);
 	
 	var indexRoutes = require('./indexRoutes')(app);
+	var loginRoutes = require('./loginRoutes')(app);
+	var categoriasRoutes = require('./categoriasRoutes')(app);
 	
 }
