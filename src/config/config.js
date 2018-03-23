7167c48e7ae666f27c40891c709d8b83a2ffe30b
root <david.diaz@zweicom.com>
2018-03-23 16:28:37 -0300

    se agregan log para configuraciones de ambientes

--- src/config/config.js
+++ src/config/config.js
@@ -15,6 +15,11 @@ let ConfigEntornoAmbiente = ConfigEntorno.EnvironmentConfig();
 //console.log( ConfigEntornoAmbiente.SERVERURL );
 
 
+console.log( "ConfigEntornoAmbiente.BD_HOST: " + ConfigEntornoAmbiente.BD_HOST );
+console.log( "ConfigEntornoAmbiente.BD_USER: " + ConfigEntornoAmbiente.BD_USER );
+console.log( "ConfigEntornoAmbiente.BD_PASS: " + ConfigEntornoAmbiente.BD_PASS );
+console.log( "ConfigEntornoAmbiente.BD_DATABASE: " + ConfigEntornoAmbiente.BD_DATABASE );
+
 
 var connection = mysql.createConnection({
         
